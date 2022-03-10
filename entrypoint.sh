dc34c813ac9e002562a37ce5aa736404c0e7a363
boss <apsu@ninga.co>
2022-03-10 19:01:57 +0800

    s

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 CONFIG=$1
 docker_run="docker run"
-docker_run="$docker_run -p 1883:1883 -v /mosquitto.conf:/mosquitto/config/mosquitto.conf -v /auth.json:/mosquitto/auth/dynamic-security.json eclipse-mosquitto"
+docker_run="$docker_run -p 1883:1883 -v ${{ github.workspace }}/.ci/mosquitto.conf:/mosquitto/config/mosquitto.conf -v ${{ github.workspace }}/.ci/auth.json:/mosquitto/auth/dynamic-security.json eclipse-mosquitto"
 # echo $docker_run
 # sleep 10000
 sh -c "$docker_run"

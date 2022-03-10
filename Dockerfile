87c1a96cc9cfd6141f4d07358034708bd5380048
boss <apsu@ninga.co>
2022-03-10 18:39:11 +0800

    s

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
 FROM docker:stable
-COPY mosquitto.conf /mosquitto.conf
-COPY auth.json /auth.json
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+CONFIG=$1
+docker_run="docker run"
 
-
-docker run -p 1883:1883 -v /mosquitto.conf:/mosquitto/config/mosquitto.conf -v /auth.json:/mosquitto/auth/dynamic-security.json eclipse-mosquitto
-
-
-echo "::mosquitto ruuunnnning"
+docker_run="$docker_run -p 1883:1883 -v /mosquitto.conf:/mosquitto/config/mosquitto.conf -v /auth.json:/mosquitto/auth/dynamic-security.json eclipse-mosquitto"
+echo $docker_run
+echo "Running $docker_run"
+sh -c "$docker_run"
+# echo "::mosquitto ruuunnnning"
